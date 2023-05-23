f115dfd0f1bba7f2728af0ae0c4955bc4d652a8d
Fatur <almaydaf.af@gmail.com>
2023-05-24 00:04:01 +0700

    add value tabel delman

--- app/Http/Controllers/DelmanController.php
+++ app/Http/Controllers/DelmanController.php
@@ -6,5 +6,24 @@ use Illuminate\Http\Request;
 
 class DelmanController extends Controller
 {
-    //
+    public function isiTabel()
+    {
+        $data = [
+            [
+                'nama' => 'Delman 1',
+                'warna' => 'Merah',
+            ],
+            [
+                'nama' => 'Delman 2',
+                'warna' => 'Biru',
+            ],
+            // Tambahkan data lain sesuai kebutuhan
+        ];
+
+        Delman::insert($data);
+
+        return "Tabel 'delman' berhasil diisi.";
+    }
+
+
 }
